1f2a5f203915b90794a6aa54fb3f577152706c20
juacrumar <juacrumar@lairen.eu>
2023-12-08 15:17:35 +0100

    implement fix by M. Bonvini

--- new_grids/run_them_all.sh
+++ new_grids/run_them_all.sh
@@ -9,19 +9,24 @@ exe=../build/Vrap
 kinfolder=input_kinematics
 output_placeholder=test.pineappl.lz4
 
+pineapploptimize() {
+    pineappl write ${output_placeholder} ${1}
+}
+
+
 ${exe} inputE605nlo.dat input_kinematics/E605.dat
-pineappl optimize ${output_placeholder} E605nlo.pineappl.lz4
+pineapploptimize  E605nlo.pineappl.lz4
 
 ## E886P
 ${exe} inputE866nlo.dat  input_kinematics/E866P.dat
-pineappl optimize ${output_placeholder} E866nlo.pineappl.lz4
+pineapploptimize  E866nlo.pineappl.lz4
 
 ## E886R
 ${exe} inputE866nlo.dat  input_kinematics/E866R.dat
-pineappl optimize ${output_placeholder} E866Rnlo.pineappl.lz4
+pineapploptimize  E866Rnlo.pineappl.lz4
 
 ${exe} inputE866deutnlo.dat  input_kinematics/E866R.dat
-pineappl optimize ${output_placeholder} E866deutRnlo.pineappl.lz4
+pineapploptimize E866deutRnlo.pineappl.lz4
 
 ## E906
 ./run_E906.sh inputE906nlo.dat
